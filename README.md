cee36cdc2554052fae52575b380175625fcd72c5
jake <jakemurphycollins@gmail.com>
2025-04-06 22:42:36 -0400

    Add Readme and edit params

---
+++ README.md
@@ -0,0 +1,40 @@
+## Installation
+To build and run the simulation and the tests, you'll need download the following packages and assign them to the PATH
+- Python (https://www.python.org/downloads/)
+- cmake (https://cmake.org/download/)
+- A C / C++ compiler (For Windows: https://www.mingw-w64.org/downloads/#w64devkit)
+
+## How To Run Unit Tests
+**_NOTE:_** The compiled build folder was included in the zip file, if you just want to run the output skip to step 4
+1. Open a terminal and navigate to the root of the project `/TillerProject`
+2. Run: `cmake --preset tests`
+3. Run: `cmake --build --preset tests`
+4. Run: `cd .\build\tests\test\ ; ctest`
+
+The output will look like the following:
+![alt text](image.png)
+The testing output shows each of the following with a pair of lines
+- The cardinal number denoting the order in which the tests were run
+  - Proper unit testing shouldn't have dependency test to test, but this is how you can keep track of if there is a dependency issue.
+- Which suite the test is from and the name of the test for identification
+  - Note the naming scheme shows conditions the unit test is creaitng and the expected output without neeidng to open the code
+- The result of the unit test
+- The execution duration of the test 
+  - This is handy to tell what conditions or functions are hogging resources
+
+## How To Run Simulation
+**_NOTE:_** The compiled build folder was included in the zip file, if you just want to run the output skip to step 4 of `To Build And Launch The Simulation`
+### To Configure The Simulation
+All of the project parameters are stored in `include/params/tiller_params.hpp`. 
+#### Key Params To Consider Messing With
+- `kTickPeriodMs`: Controls how often the tiller angle sensor runs (ie. How often the system prints to the terminal)
+- `kEncoderTicksPerDegree`: Controls the resolution of the encoder sensor
+- `kSmallestAngleChange`: Controls the smallest angle change for each angle update
+- All of the odds are. These control the odds for the tiller direction to change and the odds for the encoder to drop ticks / slip
+- 
+### To Build and Launch The Simulation
+1. Navigate to the project root `/TillerProject`
+2. Run: `cmake --preset simulation`
+3. Run: `cmake --build --preset simulation`
+4. Run: `cd .\build\bin\simulation\ ; ./simulation.exe`
+5. Kill The Simulation With: `^C`
--- include/params/tiller_params.hpp
+++ include/params/tiller_params.hpp
@@ -3,39 +3,43 @@
  *   All rights reserved.
  */
 #pragma once
-#include <stdint.h>
 
 #include <cstdint>
 #include <utility>
 
 #include "combination_sensor.hpp"
-#include "dummy_adc_driver.hpp"
-#include "dummy_encoder_driver.hpp"
 #include "encoder_sensor.hpp"
 #include "linear_mapping.hpp"
 
 namespace tiller::params
 {
 
-static constexpr float kAngleIncrementDegrees{0.5};
+// Controls the tick period of the main loop (the sensor reading loop)
 static constexpr uint64_t kTickPeriodMs{100};
+
+// Number of tasks the scheduler can hold
+static constexpr size_t kMaxNumTasks{1};
+
+// Sets the limits related to the potentiometer
 static constexpr std::pair<float, float> kPotentiometerVoltageLimits{0.0F,
                                                                      5.0F};
 static constexpr std::pair<float, float> kTillerAngleLimits{-90.0F, 90.0F};
+
+// Controls the resolution of the encoder
 static constexpr uint16_t kEncoderTicksPerDegree{100};
 
-///< The smallest angle change per iteration, note this is also the amount the
-///< encoder "slips". So if its decreased, it will take longer to accumulate
-///< encoder error and vice versa if its increased
+// The smallest angle change per iteration, note this is also the amount the
+// encoder "slips". So if its decreased, it will take longer to accumulate
+// encoder error and vice versa if its increased
 static constexpr float kSmallestAngleChange{0.1};
 
-///< 20 out of 100 odds to miss an encoder change
+// 20 out of 100 odds to miss an encoder change
 static constexpr int kOddsOutOfToDropEncoderTicks{100};
 static constexpr int kOddsToDropEncoderTicks{20};
 
-///< 15 out of 10,000 odds to change directions each time a new angle is
-///< written, changes direction often. You can reduce the odds to in order to
-///< reduce the occurance
+// 15 out of 10,000 odds to change directions each time a new angle is
+// written, changes direction often. You can reduce the odds to in order to
+// reduce the occurance
 static constexpr int kOddsOutOfToChangeDirection{10000};
 static constexpr int kOddsToChangeDirection{15};
 
@@ -54,7 +58,7 @@ static constexpr utility::LinearMapping<float>::Params
  *
  */
 static constexpr EncoderSensor::Params kTillerEncoderParams{
-    .degrees_per_encoder_tick = 0.01F,
+    .degrees_per_encoder_tick = 1 / static_cast<float>(kEncoderTicksPerDegree),
 };
 
 /**
@@ -65,8 +69,5 @@ static constexpr EncoderSensor::Params kTillerEncoderParams{
 static constexpr CombinationSensor::Params kTillerAngleSensorParams{
     .max_drift_deg = 2.0F,
 };
-
-static constexpr size_t kMaxNumTasks{
-    1};  ///< Number of tasks the scheduler can hold
 
 }  // namespace tiller::params
